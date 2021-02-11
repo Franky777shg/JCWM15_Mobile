11fb5682a2a373053d95bd3c0bc24064523b53c1
Franky777shg <frengky.sihombing.777@gmail.com>
2021-02-11 16:45:42 +0700

    intro react navigation

--- App.js
+++ App.js
@@ -1,66 +1,79 @@
 import React from 'react';
-import Axios from 'axios'
-import { FlatList, ScrollView, Text, View, ActivityIndicator, SafeAreaView } from 'react-native';
-import { Input, Icon, Button, Image } from 'react-native-elements'
+import { ScrollView, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-// import styles
-import styles from './style/unsplash'
+const Home = ({ navigation, route }) => {
+  // console.log(navigation)
+  // console.log(route)
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>This is Home</Text>
+      <Button
+        title="Go to details"
+        onPress={() => {
+          navigation.navigate('Details', {
+            name: "Budi",
+            id: 2
+          })
+        }} />
+      <Button
+        title="Go to details by push"
+        onPress={() => {
+          navigation.push('Details')
+        }} />
+      <Button
+        title="Go to Home by push"
+        onPress={() => {
+          navigation.push('Home')
+        }} />
+    </View>
+  )
+}
+
+const Details = ({ navigation, route }) => {
+  console.log('route', route)
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>This is Details</Text>
+      <Text style={{ fontSize: 20 }}>
+        Name from Home Page : {route.params ? route.params.name : 'Gak ada'}
+      </Text>
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          navigation.navigate('Home')
+        }}
+      />
+      <Button
+        title="Go to details again"
+        onPress={() => {
+          navigation.push('Details')
+        }}
+      />
+      <Button
+        title="Go to Home by push"
+        onPress={() => {
+          navigation.push('Home')
+        }}
+      />
+    </View>
+  )
+}
 
 const App = () => {
-  const [input, setInput] = React.useState("")
-  const [data, setData] = React.useState([])
-  // console.log(data)
-
-  const search = async () => {
-    console.log(input)
-    try {
-      const res = await Axios.get(`https://api.unsplash.com/search/photos?query=${input}&client_id=cIGL5ESNKJBJr4VBdND7YN_LHq_IHSt7JJom9QQreXg`)
-      setData(res.data.results)
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-
-  const MapImages = () => {
-    return (
-      data.map((item, index) => {
-        return (
-          <Image
-            containerStyle={{ margin: 5 }}
-            key={index}
-            source={{ uri: item.urls.regular }}
-            style={{ width: 155, height: 155 }}
-            PlaceholderContent={<ActivityIndicator />}
-          />
-        )
-      })
-    )
-  }
+  const Stack = createStackNavigator()
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>ONSPLASH</Text>
-      <View style={styles.searchCont}>
-        <Input
-          placeholder='Search Photos'
-          leftIcon={
-            <Icon
-              name='search'
-              size={24}
-              type='font-awesome'
-              color="#517fa4"
-            />
-          }
-          onChangeText={setInput}
-        />
-        <Button buttonStyle={{ width: 150 }} title="Search" onPress={search} />
-      </View>
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-        <MapImages />
-      </View>
-    </ScrollView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App;
+
+// Home, detail
