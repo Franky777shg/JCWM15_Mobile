f5759c3dac25b1598d8652f653960fc5f8026396
Franky777shg <frengky.sihombing.777@gmail.com>
2021-02-23 12:30:39 +0700

    update styling

--- src/screen/ProductScreen.js
+++ src/screen/ProductScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { View, Text, Dimensions, FlatList, SafeAreaView } from 'react-native'
-import { Header, Avatar, Tile, Card, Button } from 'react-native-elements'
+import { Header, Avatar, Tile, Card, Button, Rating } from 'react-native-elements'
 import Carousel from 'react-native-snap-carousel'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 // import actions
 import { getCarousel, getProduct } from '../actions'
@@ -64,34 +65,58 @@ const ProductScreen = () => {
                     loop
                 />
             </View>
-            <Text style={{ marginVertical: 5, marginHorizontal: 20, fontSize: 25, fontWeight: 'bold' }}>Product</Text>
-            {/* <SafeAreaView> */}
+            <Text style={{ marginTop: 20, marginHorizontal: 20, fontSize: 25, fontWeight: 'bold' }}>Product</Text>
             <FlatList
                 data={product}
                 renderItem={({ item }) => (
-                    <Card containerStyle={{ flex: 1, padding: 5 }}>
+                    <Card containerStyle={{ flex: 1, padding: 10, borderRadius: 5 }}>
                         <Card.Image
                             source={{
                                 uri: item.images[0]
                             }}
                             style={{ resizeMode: 'center' }}
                         />
-                        <View style={{ display: 'flex', justifyContent: 'space-between', height: 100 }}>
-                            <Text style={{ marginBottom: 10 }}>
+                        <View style={{ display: 'flex', justifyContent: 'space-between', height: 135 }}>
+                            <Text style={{ marginBottom: 10, fontSize: 12.5 }}>
                                 {item.nama}
                             </Text>
-                            <Button
-                                // icon={<Icon name='code' color='#ffffff' />}
-                                // buttonStyle={{ justifyContent: 'flex-end' }}
-                                title='VIEW NOW' />
+                            <View style={{ display: 'flex', alignItems: 'flex-start' }}>
+                                <Rating
+                                    imageSize={20}
+                                    readonly
+                                    startingValue={item.rating}
+                                />
+                                <Text style={{ marginVertical: 5 }}>IDR {item.harga.toLocaleString()}</Text>
+                                <View style={{ display: 'flex', flexDirection: 'row', width: 130, justifyContent: 'space-around' }}>
+                                    <Button
+                                        icon={
+                                            <Icon
+                                            name="bookmark"
+                                            size={15}
+                                            color="#28527a"
+                                            />
+                                        }
+                                        buttonStyle={{width: 50, backgroundColor: '#f4d160'}}
+                                    />
+                                    <Button
+                                        icon={
+                                            <Icon
+                                                name="shopping-cart"
+                                                size={15}
+                                                color="white"
+                                            />
+                                        }
+                                        buttonStyle={{width: 50, backgroundColor: '#28527a'}}
+                                        />
+                                </View>
+                            </View>
                         </View>
                     </Card>
                 )}
                 keyExtractor={(item, index) => index.toString()}
                 numColumns={2}
-                ListFooterComponent={<View style={{height: 20}}></View>}
+                ListFooterComponent={<View style={{ height: 20 }}></View>}
             />
-            {/* </SafeAreaView> */}
         </SafeAreaView>
     )
 }
