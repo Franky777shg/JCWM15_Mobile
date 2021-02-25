e8327adf15462e132cd77958a86ae18edd71169c
Franky777shg <frengky.sihombing.777@gmail.com>
2021-02-25 16:07:33 +0700

    update styling product detail

--- src/screen/ProductDetail.js
+++ src/screen/ProductDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { ScrollView, View, Text, StyleSheet, Dimensions, Pressable } from 'react-native'
-import { Button, Card } from 'react-native-elements'
+import { ScrollView, View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
+import { Button, Card, Icon } from 'react-native-elements'
 import Carousel from 'react-native-snap-carousel'
 import Modal from 'react-native-modal';
 
@@ -15,7 +15,7 @@ const COLOR = [
         name: 'RED'
     },
     {
-        color: '#12B5B4 ',
+        color: '#12B5B4',
         name: 'BLUE'
     },
     {
@@ -38,18 +38,113 @@ const ProductDetail = ({ route }) => {
     const { product } = route.params
 
     const [visible, setVisible] = React.useState(true)
+    const [color, setColor] = React.useState('')
+    const [size, setSize] = React.useState(null)
+    const [qty, setQty] = React.useState(0)
 
-    const renderColor = () => {
+    const RenderColor = () => {
         return (
             COLOR.map((item, index) => {
+                if (item.name === color) {
+                    return (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={() => setColor(item.name)}
+                            style={{
+                                backgroundColor: '#fbeeac', borderRadius: 5, height: 60,
+                                width: 60, display: 'flex', justifyContent: 'center', alignItems: 'center'
+                            }}
+                        >
+                            <View style={{
+                                borderRadius: 25,
+                                height: 50,
+                                width: 50,
+                                backgroundColor: `${item.color}`,
+                                borderColor: '#28527a',
+                                borderWidth: 2
+                            }} >
+
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }
                 return (
-                    <Pressable style={{
-                        borderRadius: 25,
-                        height: 50,
-                        width: 50,
-                        backgroundColor: `${item.color}`
-                    }}>
-                    </Pressable>
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => setColor(item.name)}
+                        style={{
+                            height: 60,
+                            width: 60, display: 'flex', justifyContent: 'center', alignItems: 'center'
+                        }}
+                    >
+                        <View style={{
+                            borderRadius: 25,
+                            height: 50,
+                            width: 50,
+                            backgroundColor: `${item.color}`,
+                            borderColor: '#28527a',
+                            borderWidth: 2
+                        }} >
+
+                        </View>
+                    </TouchableOpacity>
+                )
+            })
+        )
+
+    }
+
+    const RenderSize = () => {
+        return (
+            SIZE.map((item, index) => {
+                if (item === size) {
+                    return (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={() => setSize(item)}
+                            style={{
+                                height: 60,
+                                width: 60, display: 'flex', justifyContent: 'center', alignItems: 'center',
+                                backgroundColor: '#fbeeac', borderRadius: 5
+                            }}
+                        >
+                            <View style={{
+                                borderRadius: 25,
+                                height: 50,
+                                width: 50,
+                                borderColor: '#28527a',
+                                borderWidth: 2,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }} >
+                                <Text style={{ fontSize: 20 }}>{item}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }
+                return (
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => setSize(item)}
+                        style={{
+                            height: 60,
+                            width: 60, display: 'flex', justifyContent: 'center', alignItems: 'center'
+                        }}
+                    >
+                        <View style={{
+                            borderRadius: 25,
+                            height: 50,
+                            width: 50,
+                            borderColor: '#28527a',
+                            borderWidth: 2,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }} >
+                            <Text style={{ fontSize: 20 }}>{item}</Text>
+                        </View>
+                    </TouchableOpacity>
                 )
             })
         )
@@ -86,19 +181,62 @@ const ProductDetail = ({ route }) => {
                 title="Add to Cart"
                 onPress={() => setVisible(true)}
             />
-            <View>
-                {renderColor}
-            </View>
             <Modal
                 isVisible={visible}
                 backdropOpacity={0.1}
                 onBackdropPress={() => setVisible(false)}
-                style={{ maxHeight: 250, marginTop: 400 }}
-            // coverScreen={false}
+            // style={{ maxHeight: 250, marginTop: 400 }}
             >
-                <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
-
-
+                <View style={{ flex: 1, backgroundColor: '#ffffff', padding: 5, display: 'flex', alignItems: 'center' }}>
+                    <Text style={{ fontSize: 17 }}>Choose Color:</Text>
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        width: 315,
+                        height: 70,
+                        padding: 5,
+                        alignItems: 'center'
+                    }}
+                    >
+                        <RenderColor />
+                    </View>
+                    <Text style={{ fontSize: 17 }}>Choose Size:</Text>
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        width: 315,
+                        height: 70,
+                        padding: 5,
+                        alignItems: 'center'
+                    }}
+                    >
+                        <RenderSize />
+                    </View>
+                    <Text style={{ fontSize: 17 }}>Quantity: </Text>
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        // backgroundColor: 'salmon',
+                        width: 200,
+                        height: 70,
+                        padding: 5,
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}>
+                        <Icon
+                            raised
+                            name='minus'
+                            type='font-awesome'
+                            color='#28527a'
+                            onPress={() => console.log('hello')} />
+                        <Text style={{fontSize: 25}}>{qty}</Text>
+                        <Icon
+                            raised
+                            name='plus'
+                            type='font-awesome'
+                            color='#28527a'
+                            onPress={() => console.log('hello')} />
+                    </View>
                     <Button title="Hide modal" onPress={() => setVisible(false)} />
                 </View>
             </Modal>
